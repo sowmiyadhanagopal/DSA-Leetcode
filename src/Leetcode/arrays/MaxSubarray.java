f0f0c1d6ac4f5851cdb0b86d33a1f2ce7d499096
Your Name <sowmi@sowmya>
2025-12-17 12:01:52 +0530

    Day 2:Solved MaxSubarray,MoveZeroes and Remove Duplicate From Sorted Array problems

---
+++ src/Leetcode/arrays/MaxSubarray.java
@@ -0,0 +1,19 @@
+package Leetcode.arrays;
+
+public class MaxSubarray {
+        public int maxSubArray(int[] nums) {
+
+            int currentSum = nums[0];
+            int maxSum = nums[0];
+
+            for (int i = 1; i < nums.length; i++) {
+                currentSum = Math.max(nums[i], currentSum + nums[i]);
+                maxSum = Math.max(maxSum, currentSum);
+            }
+
+            return maxSum;
+        }
+
+    }
+
+
---
+++ src/Leetcode/arrays/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,19 @@
+package Leetcode.arrays;
+
+public class RemoveDuplicatesFromSortedArray {
+        public int removeDuplicates(int[] nums) {
+            if (nums.length == 0) return 0;
+
+            int slow = 0;
+
+            for (int fast = 1; fast < nums.length; fast++) {
+                if (nums[fast] != nums[slow]) {
+                    slow++;
+                    nums[slow] = nums[fast];
+                }
+            }
+
+            return slow + 1;
+        }
+    }
+
