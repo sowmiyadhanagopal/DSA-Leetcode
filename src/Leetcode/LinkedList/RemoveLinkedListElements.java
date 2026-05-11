2633d26c472ca7972b0ea0fc925c1e0bbfcf94cb
Sowmiya D <sowmiyadhanagopaldj@gmail.com>
2026-05-11 18:20:14 +0530

    Added RemoveLinkedListElements

---
+++ src/Leetcode/LinkedList/RemoveLinkedListElements.java
@@ -0,0 +1,26 @@
+package Leetcode.LinkedList;
+
+public class RemoveLinkedListElements {
+
+        public ListNode removeElements(ListNode head, int val) {
+            while(head!=null&&head.val==val)
+            {
+                head=head.next;
+            }
+
+            ListNode curr=head;
+            while(curr!=null&&curr.next!=null)
+            {
+                if(curr.next.val==val)
+                {
+                    curr.next=curr.next.next;
+                }
+                else
+                {
+                    curr=curr.next;
+                }
+            }
+            return head;
+        }
+    }
+
