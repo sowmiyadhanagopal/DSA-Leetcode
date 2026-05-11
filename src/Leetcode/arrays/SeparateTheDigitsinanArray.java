8cb5c82561ccb5ef6cba078d9b0b839574e3fa14
Sowmiya D <sowmiyadhanagopaldj@gmail.com>
2026-05-11 09:55:02 +0530

    Added SeparateTheDigitsinanArray

---
+++ src/Leetcode/arrays/SeparateTheDigitsinanArray.java
@@ -0,0 +1,29 @@
+package Leetcode.arrays;
+import java.util.*;
+public class SeparateTheDigitsinanArray {
+
+
+        public int[] separateDigits(int[] nums) {
+
+            List<Integer> list = new ArrayList<>();
+
+            for (int num : nums) {
+
+                String s = String.valueOf(num);
+
+                for (char ch : s.toCharArray()) {
+
+                    list.add(ch - '0');
+                }
+            }
+
+            int[] ans = new int[list.size()];
+
+            for (int i = 0; i < list.size(); i++) {
+                ans[i] = list.get(i);
+            }
+
+            return ans;
+        }
+    }
+
