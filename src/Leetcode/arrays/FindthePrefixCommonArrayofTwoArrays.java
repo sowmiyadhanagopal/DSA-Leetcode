895e842b460dea58054dae81c3c1e26631378e02
Sowmiya D <sowmiyadhanagopaldj@gmail.com>
2026-05-20 08:23:04 +0530

    Added prefixcommonarray

---
+++ src/Leetcode/arrays/FindthePrefixCommonArrayofTwoArrays.java
@@ -0,0 +1,27 @@
+package Leetcode.arrays;
+
+public class FindthePrefixCommonArrayofTwoArrays {
+
+        public int[] findThePrefixCommonArray(int[] A, int[] B) {
+            int n=A.length;
+            int[] freq=new int[n+1];
+            int[] ans=new int[n];
+            int c=0;
+            for(int i=0;i<n;i++)
+            {
+                freq[A[i]]++;
+                if(freq[A[i]]==2)
+                {
+                    c++;
+                }a
+                freq[B[i]]++;
+                if(freq[B[i]]==2)
+                {
+                    c++;
+                }
+                ans[i]=c;
+            }
+            return ans;
+        }
+    }
+
