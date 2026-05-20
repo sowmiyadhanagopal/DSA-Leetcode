e8806b2934b1604389ed527e4d5e376031ed749e
Sowmiya D <sowmiyadhanagopaldj@gmail.com>
2026-05-20 08:39:25 +0530

    Added deletenode

---
+++ src/Leetcode/LinkedList/DeleteNodeinaLinkedList.java
@@ -0,0 +1,10 @@
+package Leetcode.LinkedList;
+
+public class DeleteNodeinaLinkedList {
+
+        public void deleteNode(ListNode node) {
+            node.val=node.next.val;
+            node.next=node.next.next;
+        }
+    }
+
