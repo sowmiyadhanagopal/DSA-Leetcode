2e894f4dc5205ffda09cae69a7ead3585557a03e
Sowmiya D <sowmiyadhanagopaldj@gmail.com>
2026-05-24 08:24:55 +0530

    Added mergesortedlists

---
+++ src/Leetcode/LinkedList/MergeKSortedLists.java
@@ -0,0 +1,39 @@
+package Leetcode.LinkedList;
+import java.util.*;
+public class MergeKSortedLists {
+
+
+        public ListNode mergeKLists(ListNode[] lists) {
+            PriorityQueue<ListNode> pq =
+                    new PriorityQueue<>((a, b) -> a.val - b.val);
+            for (ListNode node : lists) {
+                if (node != null) {
+                    pq.offer(node);
+                }
+            }
+
+
+            ListNode dummy = new ListNode(-1);
+            ListNode tail = dummy;
+
+
+            while (!pq.isEmpty()) {
+
+
+                ListNode node = pq.poll();
+
+
+                tail.next = node;
+                tail = tail.next;
+
+
+                if (node.next != null) {
+                    pq.offer(node.next);
+                }
+            }
+
+            return dummy.next;
+
+        }
+    }
+
