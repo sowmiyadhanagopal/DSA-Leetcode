784a00a63a5c8a69d801d7da09c44c3840d1b333
Sowmiya D <sowmiyadhanagopaldj@gmail.com>
2026-05-25 08:55:32 +0530

    Added jumpgameVII

---
+++ src/Leetcode/arrays/JumpGameVII.java
@@ -0,0 +1,28 @@
+package Leetcode.arrays;
+
+public class JumpGameVII {
+
+        public boolean canReach(String s, int minJump, int maxJump) {
+            int n=s.length();
+            boolean[] reachable=new boolean[n];
+            reachable[0]=true;
+            int reachablecount=0;
+            for(int i=1;i<n;i++)
+            {
+                if(i-minJump>=0 && reachable[i-minJump])
+                {
+                    reachablecount++;
+                }
+                if(i-maxJump-1>=0 && reachable[i-maxJump-1])
+                {
+                    reachablecount--;
+                }
+                if(s.charAt(i)=='0'&& reachablecount>0)
+                {
+                    reachable[i]=true;
+                }
+            }
+            return reachable[n-1];
+        }
+    }
+
