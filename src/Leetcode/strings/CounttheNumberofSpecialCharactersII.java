c03b92afedb4196a854e30930834f605ce48ea59
Sowmiya D <sowmiyadhanagopaldj@gmail.com>
2026-05-27 10:04:32 +0530

    Added specialII

---
+++ src/Leetcode/strings/CounttheNumberofSpecialCharactersII.java
@@ -0,0 +1,49 @@
+package Leetcode.strings;
+
+public class CounttheNumberofSpecialCharactersII {
+
+        public int numberOfSpecialChars(String word) {
+
+            int[] lastLower = new int[26];
+            int[] firstUpper = new int[26];
+
+            for(int i = 0; i < 26; i++) {
+                lastLower[i] = -1;
+                firstUpper[i] = -1;
+            }
+
+            for(int i = 0; i < word.length(); i++) {
+
+                char ch = word.charAt(i);
+
+
+                if(Character.isLowerCase(ch)) {
+                    lastLower[ch - 'a'] = i;
+                }
+
+
+                else {
+                    int idx = ch - 'A';
+
+                    if(firstUpper[idx] == -1) {
+                        firstUpper[idx] = i;
+                    }
+                }
+            }
+
+            int count = 0;
+
+            for(int i = 0; i < 26; i++) {
+
+                if(lastLower[i] != -1 && firstUpper[i] != -1) {
+
+                    if(lastLower[i] < firstUpper[i]) {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+
