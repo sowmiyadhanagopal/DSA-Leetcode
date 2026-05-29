c27ec8df30372faae1db15185555de00a2002ecc
Sowmiya D <sowmiyadhanagopaldj@gmail.com>
2026-05-29 09:39:48 +0530

    Added linkedlistcycleII

---
+++ src/Leetcode/LinkedList/LinkedListCycleII.java
@@ -0,0 +1,27 @@
+package Leetcode.LinkedList;
+
+public class LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null&&fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast)
+            {
+
+                ListNode ptr = head;
+
+                while (ptr != slow) {
+                    ptr = ptr.next;
+                    slow = slow.next;
+                }
+
+                return ptr;
+            }
+
+        }
+        return null;
+    }
+}
