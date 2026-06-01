edae71044cdf39bd6d164f86c978c645daadc060
Sowmiya D <sowmiyadhanagopaldj@gmail.com>
2026-06-01 13:08:08 +0530

    Added candies

---
+++ src/Leetcode/arrays/MinimumCostofBuyingCandiesWithDiscount.java
@@ -0,0 +1,18 @@
+package Leetcode.arrays;
+import java.util.*;
+public class MinimumCostofBuyingCandiesWithDiscount {
+
+
+        public int minimumCost(int[] cost) {
+            Arrays.sort(cost);
+            int res = 0;
+            int n = cost.length;
+            for (int i = n - 1; i >= 0; --i) {
+                if ((n - 1 - i) % 3 != 2) {
+                    res += cost[i];
+                }
+            }
+            return res;
+        }
+    }
+
