c3d04a9b273e3a03869ca390a5cee832d4985373
Sowmiya D <sowmiyadhanagopaldj@gmail.com>
2026-06-14 10:08:41 +0530

    Added wordmapping

---
+++ src/Leetcode/strings/WeightedWordMapping.java
@@ -0,0 +1,22 @@
+package Leetcode.strings;
+
+public class WeightedWordMapping {
+
+        public String mapWordWeights(String[] words, int[] weights) {
+            StringBuilder ans = new StringBuilder();
+
+            for (String word : words) {
+                int sum = 0;
+
+                for (char ch : word.toCharArray()) {
+                    sum += weights[ch - 'a'];
+                }
+
+                int mod = sum % 26;
+                ans.append((char) ('z' - mod));
+            }
+
+            return ans.toString();
+        }
+    }
+
