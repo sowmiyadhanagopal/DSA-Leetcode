a210284b26eca80bbe016428fbec904e1c93b971
Sowmiya D <sowmiyadhanagopaldj@gmail.com>
2026-07-03 18:30:52 +0530

    Added arraywithstackoperations

---
+++ src/Leetcode/stack/BuildanArrayWithStackOperations.java
@@ -0,0 +1,22 @@
+package Leetcode.stack;
+import java.util.*;
+public class BuildanArrayWithStackOperations {
+
+        public List<String> buildArray(int[] target, int n) {
+            List<String> ans = new ArrayList();
+            int i = 0;
+
+            for (int num : target) {
+                while (i < num - 1) {
+                    ans.add("Push");
+                    ans.add("Pop");
+                    i++;
+                }
+
+                ans.add("Push");
+                i++;
+            }
+
+            return ans;
+        }
+    }
