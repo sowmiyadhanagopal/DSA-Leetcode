9c15f955fdb2fe81ba1bea27ee1c2a91cc792890
Sowmiya D <sowmiyadhanagopaldj@gmail.com>
2026-07-09 06:02:44 +0530

    Added 132 pattern

---
+++ src/Leetcode/stack/Pattern132.java
@@ -0,0 +1,26 @@
+package Leetcode.stack;
+import java.util.*;
+public class Pattern132 {
+    class Solution {
+        public boolean find132pattern(int[] nums) {
+
+            Stack<Integer> st = new Stack<>();
+            int second = Integer.MIN_VALUE;
+
+            for (int i = nums.length - 1; i >= 0; i--) {
+
+                if (nums[i] < second) {
+                    return true;
+                }
+
+                while (!st.isEmpty() && nums[i] > st.peek()) {
+                    second = st.pop();
+                }
+
+                st.push(nums[i]);
+            }
+
+            return false;
+        }
+    }
+}
