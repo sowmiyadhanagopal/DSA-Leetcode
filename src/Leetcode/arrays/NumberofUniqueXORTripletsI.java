9b3805246b6c27b3bd768f37bcc3d3f67f60e8d3
Sowmiya D <sowmiyadhanagopaldj@gmail.com>
2026-07-23 16:48:00 +0530

    Added xortriplets

---
+++ src/Leetcode/arrays/NumberofUniqueXORTripletsI.java
@@ -0,0 +1,20 @@
+package Leetcode.arrays;
+
+public class NumberofUniqueXORTripletsI {
+    class Solution {
+        public int uniqueXorTriplets(int[] nums) {
+            int n=nums.length;
+            if(n<=2)
+            {
+                return n;
+            }
+            int ans=1;
+            while(ans<=n)
+
+            {
+                ans<<=1;
+            }
+            return ans;
+        }
+    }
+}
