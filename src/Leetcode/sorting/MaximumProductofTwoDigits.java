2673fa2d26a9304846727940aa887e0ab09d409a
Sowmiya D <sowmiyadhanagopaldj@gmail.com>
2026-07-25 08:29:02 +0530

    Added maxproduct

---
+++ src/Leetcode/sorting/MaximumProductofTwoDigits.java
@@ -0,0 +1,25 @@
+package Leetcode.sorting;
+
+public class MaximumProductofTwoDigits {
+    class Solution {
+        public int maxProduct(int n) {
+            int max1=0;
+            int max2=0;
+            while(n>0)
+            {
+                int d=n%10;
+                if(d>max1)
+                {
+                    max2=max1;
+                    max1=d;
+                }
+                else if(d>max2)
+                {
+                    max2=d;
+                }
+                n=n/10;
+            }
+            return max1*max2;
+        }
+    }
+}
